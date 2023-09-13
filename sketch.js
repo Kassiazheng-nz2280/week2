b14a809e0b529c772c222ec94aa02a2ac91507ff
Kassia Zheng <nz2280@nyu.edu>
2023-09-13 15:24:04 -0400

    added two ellipsis and one square

--- sketch.js
+++ sketch.js
@@ -1,5 +1,15 @@
 function setup() {
+    createCanvas(600, 600, );
+    background(230, 93, 4);
 }
 
 function draw() {
+    stroke(0);
+    ellipse(79, 79, 100, [100]);
+    fill('pink');
+    ellipse(300, 300, 200, [200]);
+    fill('yellow');
+    stroke(255, 33, 44);
+    strokeWeight (5);
+    rect(300, 300, 200, 200);
 }
